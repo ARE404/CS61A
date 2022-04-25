419ccda7b6a52309646d50fa26f73e611f43c6f0
404ARE <936233544@qq.com>
2022-04-25 23:13:01 +0800

    finished lab00, cna't use parsons

--- lab/lab00/lab00.py
+++ lab/lab00/lab00.py
@@ -1,8 +1,8 @@
-def twenty_eighteen():
-    """Come up with the most creative expression that evaluates to 2018,
+def twenty_twenty_two():
+    """Come up with the most creative expression that evaluates to 2022,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_eighteen()
-    2018
+    >>> twenty_twenty_two()
+    2022
     """
-    return 32*32*2-30
+    return pow(2,10)*2-26
