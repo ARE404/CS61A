5662a804000968dba67f896716b317ab640cea84
404ARE <936233544@qq.com>
2022-04-26 20:36:39 +0800

    finished hw02 parsons problems

--- HW/hw02/hw02.py
+++ HW/hw02/hw02.py
@@ -1,23 +1,44 @@
-HW_SOURCE_FILE = 'hw02.py'
+from operator import add, mul
 
-def square(x):
-    return x * x
+square = lambda x: x * x
 
-def identity(x):
-    return x
+identity = lambda x: x
 
 triple = lambda x: 3 * x
 
 increment = lambda x: x + 1
 
-add = lambda x, y: x + y
 
-mul = lambda x, y: x * y
+HW_SOURCE_FILE = __file__
+
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
 
 def product(n, term):
     """Return the product of the first n terms in a sequence.
-    n    -- a positive integer
-    term -- a function that takes one argument
+
+    n: a positive integer
+    term:  a function that takes one argument to produce the term
 
     >>> product(3, identity)  # 1 * 2 * 3
     6
@@ -32,36 +53,65 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    i = 0
-    total = 0
-    while i <= n:
-        total *= term(i)
-    return total
+    "*** YOUR CODE HERE ***"
 
-# The identity function, defined using a lambda expression!
-identity = lambda k: k
 
-def factorial(n):
-    """Return n factorial for n >= 0 by calling product.
+def accumulate(merger, start, n, term):
+    """Return the result of merging the first n terms in a sequence and start.
+    The terms to be merged are term(1), term(2), ..., term(n). merger is a
+    two-argument commutative function.
 
-    >>> factorial(4)
-    24
-    >>> factorial(6)
-    720
-    >>> from construct_check import check
-    >>> check(HW_SOURCE_FILE, 'factorial', ['Recursion', 'For', 'While'])
-    True
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    >>> # ((2 * 1^2 * 2) * 2^2 * 2) * 3^2 * 2
+    >>> accumulate(lambda x, y: 2 * x * y, 2, 3, square)
+    576
+    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
+    16
     """
     "*** YOUR CODE HERE ***"
 
-def make_adder(n):
-    """Return a function that takes an argument K and returns N + K.
 
-    >>> add_three = make_adder(3)
-    >>> add_three(1) + add_three(2)
-    9
-    >>> make_adder(1)(2)
-    3
+def summation_using_accumulate(n, term):
+    """Returns the sum: term(1) + ... + term(n), using accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    >>> # You aren't expected to understand the code of this test.
+    >>> # Check that the bodies of the functions are just return statements.
+    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
     """
     "*** YOUR CODE HERE ***"
-    return 'REPLACE ME'
+
+
+def product_using_accumulate(n, term):
+    """Returns the product: term(1) * ... * term(n), using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    >>> # You aren't expected to understand the code of this test.
+    >>> # Check that the bodies of the functions are just return statements.
+    >>> # If this errors, make sure you have removed the "***YOUR CODE HERE***".
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
+    """
+    "*** YOUR CODE HERE ***"
---
+++ HW/hw02/parsons_probs/filter_sequence.py
@@ -0,0 +1,19 @@
+def filter_sequence(cond, start, stop):
+    """
+    Returns the sum of numbers from start (inclusive) to stop (inclusive) that satisfy
+    the one-argument function cond.
+
+    >>> filter_sequence(lambda x: x % 2 == 0, 0, 10) # .Case 1
+    30
+    >>> filter_sequence(lambda x: x % 2 == 1, 0, 10) # .Case 2
+    25
+    """
+    sum = 0
+    x = start
+    while x<=stop:
+        if cond(x):
+            sum += x
+        x += 1
+    return sum
+
+
