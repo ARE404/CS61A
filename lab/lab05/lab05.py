54b2a144588de900f96970edd703a93e22fa84bd
404ARE <936233544@qq.com>
2022-05-15 19:40:52 +0800

    finished lab05

---
+++ lab/lab05/lab05.py
@@ -0,0 +1,110 @@
+HW_SOURCE_FILE = __file__
+
+
+def flatten(s):
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    """
+    # outputlist=[]
+    # for i in range(len(s)):
+    #     if type(s[i])==list:
+    #         outputlist.append(flatten(s[i]))
+    #     else:
+    #         outputlist.append(s[i])
+    # return outputlist
+    # if type(s)==list:
+    #     for i in range(len(s)):
+    #         return flatten(s[i])
+    # else:
+    #     return s
+    if not s:
+        return []
+
+    if type(s[0])==list:
+        return flatten(s[0])+flatten(s[1:])
+    else:
+        return [s[0]]+flatten(s[1:])
+
+def couple(s, t):
+    """Return a list of two-element lists in which the i-th element is [s[i], t[i]].
+
+    >>> a = [1, 2, 3]
+    >>> b = [4, 5, 6]
+    >>> couple(a, b)
+    [[1, 4], [2, 5], [3, 6]]
+    >>> c = ['c', 6]
+    >>> d = ['s', '1']
+    >>> couple(c, d)
+    [['c', 's'], [6, '1']]
+    """
+    assert len(s) == len(t)
+    outputlist=[]
+    for index in range(len(s)):
+        outputlist+=[[s[index],t[index]]]
+    return outputlist
+
+def insert_items(lst, entry, elem):
+    """Inserts elem into lst after each occurence of entry and then returns lst.
+
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> double_lst = [1, 2, 1, 2, 3, 3]
+    >>> double_lst = insert_items(double_lst, 3, 4)
+    >>> double_lst
+    [1, 2, 1, 2, 3, 4, 3, 4]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    >>> # Ban creating new lists
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'insert_items',
+    ...       ['List', 'ListComp', 'Slice'])
+    True
+    """
+    # if is the index increased, skip it
+    count=0
+    # check if equal to entry
+    for index,item in enumerate(lst):
+        if count==1:
+            count=0
+            continue
+        if item==entry:
+            lst.insert(index+1,elem)
+            count=1
+    # for index in range(len(lst)+1):
+    #     if lst[index]==entry:
+    #         lst.insert(index+1,elem)
+    #         index+=1
+    return lst
+
+def change_abstraction(change):
+    """
+    For testing purposes.
+    >>> change_abstraction(True)
+    >>> change_abstraction.changed
+    True
+    """
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
