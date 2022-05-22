3307119854e8fd7b9b5b77478dfa4f052d599e60
404ARE <936233544@qq.com>
2022-05-22 13:54:27 +0800

    fixed a little bug in project ants

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -474,7 +474,7 @@ class Water(Place):
 # BEGIN Problem 11
 # The ScubaThrower class
 class ScubaThrower(ThrowerAnt):
-    name='ScubaThrower'
+    name='Scuba'
     # implemented=True
     is_waterproof=True
     food_cost=6
